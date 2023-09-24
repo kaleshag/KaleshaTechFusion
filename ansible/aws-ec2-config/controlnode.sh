c23bdabe74f7a36f277741c9527f1ff8c144d1d2
kaleshag <gagguturu.kalesha@gmail.com>
2023-09-24 14:29:47 +0530

    updated the scripts

--- ansible/aws-ec2-config/controlnode.sh
+++ ansible/aws-ec2-config/controlnode.sh
@@ -13,4 +13,4 @@ echo "$username:$password" | sudo chpasswd -e
 echo "$username ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 #-------------------Customizing the PS1 Terminal--------------------------------
 custom_ps='PS1="\[\e[31m\]\u\[\e[34m\]@\[\e[37m\]\h\[\e[m\]\[\e[1;32m\][\d][\@]\[\e[m\]:\[\e[90m\]\W\[\e[m\e[37m\]\$\[\e[m\]"'
-echo "custom_ps" >> "/home/$username/.bashrc"
+echo "$custom_ps" >> "/home/$username/.bashrc"
